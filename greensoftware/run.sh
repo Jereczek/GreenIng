015eeef09ddbc7dd0ffdac215e140291215995f8
jereczekm <michal.jereczek@kainos.com>
2023-05-14 13:33:21 +0200

    Unblock memory

--- greensoftware/run.sh
+++ greensoftware/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "STARTING APPLICATION"
-java -jar build/libs/greensoftware-1.0-all.jar
+java -jar build/libs/greensoftware-1.0-all.jar -Xms1024m -Xmx3072m
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "STARTING APPLICATION"
-java -jar greensoftware/build/libs/greensoftware-1.0-all.jar
+java -jar greensoftware/build/libs/greensoftware-1.0-all.jar -Xms1024m -Xmx3072m
